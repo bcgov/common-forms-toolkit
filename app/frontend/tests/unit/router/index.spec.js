6354faed5e85a0127278c54fc1fe1b63b3bb3ab5
Lucas O'Neil <lucasoneil@gmail.com>
2020-05-13 16:46:48 -0700

    test

--- app/frontend/tests/unit/router/index.spec.js
+++ app/frontend/tests/unit/router/index.spec.js
@@ -4,13 +4,12 @@ describe('Router', () => {
   const routes = router.options.routes;
 
   it('has the correct number of routes', () => {
-    expect(routes).toHaveLength(3);
+    expect(routes).toHaveLength(4);
   });
 
   it('has the expected routes', () => {
     const routeSet = new Set(routes);
     expect(routeSet).toContainEqual(expect.objectContaining({ name: 'Home' }));
-    expect(routeSet).toContainEqual(expect.objectContaining({ name: 'Secure' }));
     expect(routeSet).toContainEqual(expect.objectContaining({ name: 'NotFound' }));
   });
 });
